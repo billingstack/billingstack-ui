a0ba526a4a122c1bbda6ffdec513fcee27079e5f
sp <sp@sps-MacBook-Pro.local>
2013-06-06 22:48:47 +0200

    use minified versions in production

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,76 +1,103 @@
+'use strict';
+
 var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+
 var mountFolder = function (connect, dir) {
     return connect.static(require('path').resolve(dir));
 };
 
 
 /*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['app/js/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    connect: {
-        options: {
-            port: 9000,
-            //base: 'app'
-        },
-        livereload: {
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+          '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+          '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+          ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        jshint: {
             options: {
-                middleware: function(connect) {
-                    return [
-                        lrSnippet,
-                        mountFolder(connect, 'app')
-                    ]
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                //'app/js/{,*/}*.js',
+                //'!app/js/lib/*'
+            ]
+        },
+        // Task configuration.
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: true
+            },
+            dist: {
+                src: ['app/js/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'dist/<%= pkg.name %>.min.js'
+            }
+        },
+        useminPrepare: {
+            html: 'app/index.html',
+            options: {
+                dest: 'dist/app'
+            }
+        },
+        usemin: {
+            html: ['dist/{,*/}*.html'],
+            css: ['dist/css/{,*/}*.css'],
+            options: {
+                dirs: ['dist/app']
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                //base: 'app'
+            },
+            livereload: {
+                options: {
+                    middleware: function(connect) {
+                        return [
+                            lrSnippet,
+                            mountFolder(connect, 'app')
+                        ]
+                    }
                 }
             }
+        },
+        watch: {
+            livereload: {
+                options: {
+                    livereload: true
+                },
+                files: [
+                    'app/*.html'
+                ]
+            }
         }
-    },
-    watch: {
-        livereload: {
-            options: {
-                livereload: true
-            },
-            files: [
-                'app/*.html'
-            ]
-        }
-    }
-  });
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task.
-  grunt.registerTask('default', ['concat', 'uglify', 'connect', 'watch']);
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'connect', 'watch']);
 
 };
