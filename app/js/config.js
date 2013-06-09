73bbe4b53af7eb5a38e1fdbaa69e4f7b970359f4
Endre Karlson <endre.karlson@gmail.com>
2013-06-09 12:09:33 +0300

    Update config.js
    Make it more config easy.

--- app/js/config.js
+++ app/js/config.js
@@ -1,6 +1,11 @@
+var proto = 'http'
+var host = 'localhost';
+var port = '9091';
+var path = '/v1'
+
 angular.element(document).ready(function() {
-	billingstack.value('config', {
-		endpoint : 'http://localhost\\:8080/billingstack-api'
-	})
-	angular.bootstrap(document, ['billingstack']);
+        billingstack.value('config', {
+                endpoint : proto + '://' + host + '\\:' + port + path
+        })
+        angular.bootstrap(document, ['billingstack']);
 });
